af1a79454e984568a1202185df6cd559e1ad4da9
Andreas Olsson <olsson.andreas@gmail.com>
2022-03-30 07:46:39 +0200

    GetMember implemented

--- client/member.go
+++ client/member.go
@@ -2,59 +2,45 @@ package client
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 
 	"github.com/jawee/ir-webapi-client/client/response"
 )
 
-func (c *Client) GetMember(custId int) error {
-    uri := "https://members-ng.iracing.com/data/member/get"
+func (c *Client) GetMember(custId int) (*response.MemberResponse, error) {
+	uri := fmt.Sprintf("https://members-ng.iracing.com/data/member/get?cust_ids=%d", custId)
 
-    c.Client.Jar = c.CookieJar
+    linkResp, err := c.getLink(uri)
+	if err != nil {
+		return nil, err
+	}
 
-    req, err := http.NewRequest("GET", uri, nil)
-
-    if err != nil {
-        return err
-    }
-
-    req.URL.RawQuery = fmt.Sprintf("cust_ids=%d", custId)
-
-    resp, err := c.Client.Do(req)
+    var memberResponse response.MemberResponse
+    err = c.fetchLink(linkResp.Link, &memberResponse)
 
     if err != nil {
-        return err
+        return nil, err
     }
 
-    defer resp.Body.Close()
-
-    b, err := io.ReadAll(resp.Body)
-
-    if err != nil {
-        log.Fatalln(err)
-    }
-
-    fmt.Println(string(b))
-
-    return nil;
+    return &memberResponse, nil
 }
 
 func (c *Client) GetMemberRecentRaces(custId int) (*response.RecentRacesResponse, error) {
-    uri := fmt.Sprintf("https://members-ng.iracing.com/data/stats/member_recent_races?cust_id=%d", custId)
+	uri := fmt.Sprintf("https://members-ng.iracing.com/data/stats/member_recent_races?cust_id=%d", custId)
 
-    linkResp, err := c.getLink(uri) 
-    if err != nil {
-        log.Fatalln(err)
-    }
+	linkResp, err := c.getLink(uri)
+	if err != nil {
+		log.Fatalln(err)
+        return nil, err
+	}
 
-    var memberRecentRacesResp response.RecentRacesResponse
-    err = c.fetchLink(linkResp.Link, &memberRecentRacesResp)
+	var memberRecentRacesResp response.RecentRacesResponse
+	err = c.fetchLink(linkResp.Link, &memberRecentRacesResp)
 
-    if err != nil {
-        log.Fatalln(err)
-    }
+	if err != nil {
+		log.Fatalln(err)
+        return nil, err
+	}
 
-    return &memberRecentRacesResp, nil;
+	return &memberRecentRacesResp, nil
 }
